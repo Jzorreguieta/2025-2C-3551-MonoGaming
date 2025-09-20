33fa2e210bf56464890f474d7f9cec5c0dff2951
Guido Moreyra <guido98jm@gmail.com>
2025-09-20 18:44:59 -0300

    Se agrega un modulo basico de pasillo  para  ejemplo

---
+++ TGC.MonoGame.TP/Models/Modules/BasicModule.cs
@@ -0,0 +1,55 @@
+
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace  TGC.MonoGame.TP;
+
+internal class BasicModule
+{
+    private Matrix _worldMatrix;
+    private Model _model;
+    private Effect _effect;
+
+    public BasicModule(ContentManager content, string contentFolder3D, string contentFolderEffects, Matrix worldMatrix)
+    {
+
+        var rotation = Matrix.CreateRotationY(MathHelper.ToRadians(90));
+
+        _worldMatrix = worldMatrix * rotation;
+        _model = content.Load<Model>(contentFolder3D + "Modules/BasicModule/Model/Gang_grade001");
+        _effect = content.Load<Effect>(contentFolderEffects + "BasicShader");
+        _effect.Parameters["DiffuseColor"].SetValue(Color.DarkBlue.ToVector3());
+
+
+        foreach (var mesh in _model.Meshes)
+        {
+            // Un mesh puede tener mas de 1 mesh part (cada 1 puede tener su propio efecto).
+            foreach (var meshPart in mesh.MeshParts)
+            {
+                meshPart.Effect = _effect;
+            }
+        }
+    }
+
+
+    public void Draw(Matrix view, Matrix projection)
+    {
+        float scale = 0.1f;
+        // Para dibujar le modelo necesitamos pasarle informacion que el efecto esta esperando.
+        _effect.Parameters["View"].SetValue(view);
+        _effect.Parameters["Projection"].SetValue(projection);
+        foreach (var mesh in _model.Meshes)
+        {
+        
+            var meshWorld = mesh.ParentBone.Transform;
+            var scaleMatrix = Matrix.CreateScale(scale);
+            // We set the main matrices for mesh to draw.
+            _effect.Parameters["World"].SetValue(meshWorld * _worldMatrix * scaleMatrix);
+
+            // Draw the mesh.
+            mesh.Draw();
+        }
+    }
+}
--- TGC.MonoGame.TP/TGCGame.cs
+++ TGC.MonoGame.TP/TGCGame.cs
@@ -1,15 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
-namespace TGC.MonoGame.TP;
+namespace  TGC.MonoGame.TP;
 
-/// <summary>
-///     Esta es la clase principal del juego.
-///     Inicialmente puede ser renombrado o copiado para hacer mas ejemplos chicos, en el caso de copiar para que se
-///     ejecute el nuevo ejemplo deben cambiar la clase que ejecuta Program <see cref="Program.Main()" /> linea 10.
-/// </summary>
 public class TGCGame : Game
 {
     public const string ContentFolder3D = "Models/";
@@ -22,16 +19,15 @@ public class TGCGame : Game
     private readonly GraphicsDeviceManager _graphics;
 
     private Effect _effect;
-    private Model _model;
     private Matrix _projection;
-    private float _rotation;
     private SpriteBatch _spriteBatch;
     private Matrix _view;
     private Matrix _world;
 
-    /// <summary>
-    ///     Constructor del juego.
-    /// </summary>
+    //Modulo de prueba. Mas adelante deberia ser reemplazado por una lista de modulos
+    //Deberia usara polimorfismo.
+    private List<BasicModule> basicModule;
+
     public TGCGame()
     {
         // Maneja la configuracion y la administracion del dispositivo grafico.
@@ -47,113 +43,68 @@ public class TGCGame : Game
         IsMouseVisible = true;
     }
 
-    /// <summary>
-    ///     Se llama una sola vez, al principio cuando se ejecuta el ejemplo.
-    ///     Escribir aqui el codigo de inicializacion: el procesamiento que podemos pre calcular para nuestro juego.
-    /// </summary>
+
     protected override void Initialize()
     {
-        // La logica de inicializacion que no depende del contenido se recomienda poner en este metodo.
-
-        // Apago el backface culling.
-        // Esto se hace por un problema en el diseno del modelo del logo de la materia.
-        // Una vez que empiecen su juego, esto no es mas necesario y lo pueden sacar.
-        var rasterizerState = new RasterizerState();
-        rasterizerState.CullMode = CullMode.None;
-        GraphicsDevice.RasterizerState = rasterizerState;
-        // Seria hasta aca.
-
-        // Configuramos nuestras matrices de la escena.
         _world = Matrix.Identity;
-        _view = Matrix.CreateLookAt(Vector3.UnitZ * 150, Vector3.Zero, Vector3.Up);
+        _view = Matrix.CreateLookAt(new Vector3(0, 0, 300), Vector3.Zero, Vector3.Up);
         _projection =
-            Matrix.CreatePerspectiveFieldOfView(MathHelper.PiOver4, GraphicsDevice.Viewport.AspectRatio, 1, 250);
+            Matrix.CreatePerspectiveFieldOfView(MathHelper.PiOver4, GraphicsDevice.Viewport.AspectRatio, 1, 2500);
 
         base.Initialize();
     }
 
-    /// <summary>
-    ///     Se llama una sola vez, al principio cuando se ejecuta el ejemplo, despues de Initialize.
-    ///     Escribir aqui el codigo de inicializacion: cargar modelos, texturas, estructuras de optimizacion, el procesamiento
-    ///     que podemos pre calcular para nuestro juego.
-    /// </summary>
     protected override void LoadContent()
     {
-        // Aca es donde deberiamos cargar todos los contenido necesarios antes de iniciar el juego.
+        //No se que es esto si alguien lo averigua cuenteme. Att:Guido
         _spriteBatch = new SpriteBatch(GraphicsDevice);
 
-        // Cargo el modelo del logo.
-        _model = Content.Load<Model>(ContentFolder3D + "tgc-logo/tgc-logo");
+    
 
         // Cargo un efecto basico propio declarado en el Content pipeline.
         // En el juego no pueden usar BasicEffect de MG, deben usar siempre efectos propios.
         _effect = Content.Load<Effect>(ContentFolderEffects + "BasicShader");
 
-        // Asigno el efecto que cargue a cada parte del mesh.
-        // Un modelo puede tener mas de 1 mesh internamente.
-        foreach (var mesh in _model.Meshes)
+        // Cargo los modelos aca abajo utilizando el effect de arriba.
+
+        basicModule = new List<BasicModule>
         {
-            // Un mesh puede tener mas de 1 mesh part (cada 1 puede tener su propio efecto).
-            foreach (var meshPart in mesh.MeshParts)
-            {
-                meshPart.Effect = _effect;
-            }
-        }
+            new BasicModule(Content, ContentFolder3D, ContentFolderEffects, Matrix.Identity * Matrix.CreateTranslation(Vector3.Left*100f)),
+
+        };//ContentManager content, string contentFolder3D, string contentFolderEffects
 
         base.LoadContent();
     }
 
-    /// <summary>
-    ///     Se llama en cada frame.
-    ///     Se debe escribir toda la logica de computo del modelo, asi como tambien verificar entradas del usuario y reacciones
-    ///     ante ellas.
-    /// </summary>
+
     protected override void Update(GameTime gameTime)
     {
-        // Aca deberiamos poner toda la logica de actualizacion del juego.
 
-        // Capturar Input teclado
         if (Keyboard.GetState().IsKeyDown(Keys.Escape))
         {
-            //Salgo del juego.
             Exit();
         }
-
-        // Basado en el tiempo que paso se va generando una rotacion.
-        _rotation += Convert.ToSingle(gameTime.ElapsedGameTime.TotalSeconds);
-
-        _world = Matrix.CreateRotationY(_rotation);
 
         base.Update(gameTime);
     }
 
-    /// <summary>
-    ///     Se llama cada vez que hay que refrescar la pantalla.
-    ///     Escribir aqui el codigo referido al renderizado.
-    /// </summary>
     protected override void Draw(GameTime gameTime)
     {
-        // Aca deberiamos poner toda la logia de renderizado del juego.
-        GraphicsDevice.Clear(Color.Black);
-
-        // Para dibujar le modelo necesitamos pasarle informacion que el efecto esta esperando.
-        _effect.Parameters["View"].SetValue(_view);
-        _effect.Parameters["Projection"].SetValue(_projection);
-        _effect.Parameters["DiffuseColor"].SetValue(Color.DarkBlue.ToVector3());
-
-        foreach (var mesh in _model.Meshes)
+        //El fonde del mundo es balnco.
+        GraphicsDevice.Clear(Color.White);
+        foreach (BasicModule module in basicModule)
         {
-            _effect.Parameters["World"].SetValue(mesh.ParentBone.Transform * _world);
-            mesh.Draw();
+            module.Draw(_view,_projection);
         }
+        
+        //Cada modelo deberia tener su propio draw.
+        //A menos que sea para prueba no deberian haber dibujos en este metodo
     }
 
-    /// <summary>
-    ///     Libero los recursos que se cargaron en el juego.
-    /// </summary>
+
     protected override void UnloadContent()
     {
-        // Libero los recursos.
+
         Content.Unload();
 
         base.UnloadContent();
